99df1e5c2bf5ef6101a3a2b36c7784d1a74c4a77
DeerDancer5 <80452@student.pb.edu.pl>
2024-01-17 21:33:56 +0100

    Implemented JavaFX media player & controls

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module org.example.mp3playerfx {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.media;
     requires org.kordamp.bootstrapfx.core;
 
     opens org.example.mp3playerfx to javafx.fxml;
